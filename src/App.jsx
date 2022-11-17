3033bc0b0a6a25f8d1662eb78aa8626643e58c95
Andrew <andriy.vikuk@gmail.com>
2022-11-17 12:41:42 +0200

    New Branch

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [data, setData] = useState({});
   const [loсation, setLocation] = useState("");
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=Uzhhorod&units=imperial&appid=a3dc501f090463d7c22b1396b8dcf784`
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=a3dc501f090463d7c22b1396b8dcf784`
   function callData(){
   axios.get(url).then((response) => {
     setData(response.data)
@@ -18,8 +18,8 @@ function App() {
     <button onClick={() => callData()}>Click</button>
      <p>{data.name}</p>
      <p>{data.main.temp} F</p>
-
      <p>{data.wind.speed}/ ms</p>
+     <p>{data.timezone}</p>
     </div>
   );
 }
