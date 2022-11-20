896e84edf12f43e936e7f23815efb5b7b7789902
Andrew <andriy.vikuk@gmail.com>
2022-11-20 13:26:41 +0200

    Preloader

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ function App() {
 
   const [data, setData] = useState({});
   const [loсation, setLocation] = useState("");
+  const [loading, setLoading] = useState(true);
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${loсation}&units=imperial&appid=a3dc501f090463d7c22b1396b8dcf784`
   const searchLocation = (event) => {
@@ -28,48 +29,48 @@ function App() {
     }
   }
 
-
-
-  var today = new Date(),
-
-  time = today.getHours();
-console.log(time)
-
-
+  const spinner = document.getElementById("spinner");
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = "none";
+      setLoading(false);
+    }, 2000);
+  }
 
   return (
+    !loading && (
     <div className="App pt-14">
       <div className="main">
-      <input
-          className="border-2 border-grey-400 rounded-xl px-6 py-2"
-          value={loсation}
-          onChange={event => setLocation(event.target.value)}
-          onKeyPress={searchLocation}
-          placeholder='Enter Location'
-          type="text" />
-          {data.weather ? project(data.weather[0].main) : null}
-          <h2 className="mb-6">{data.name}</h2>
-          {data.main ? <h1>{(5/9 * (data.main.temp - 32)).toFixed(0)} °C</h1> : null}
+        <input
+            className="border-2 border-grey-400 rounded-xl px-6 py-2"
+            value={loсation}
+            onChange={event => setLocation(event.target.value)}
+            onKeyPress={searchLocation}
+            placeholder='Enter Location'
+            type="text" />
+            {data.weather ? project(data.weather[0].main) : null}
+            <h2 className="mb-6">{data.name}</h2>
+            {data.main ? <h1>{(5/9 * (data.main.temp - 32)).toFixed(0)} °C</h1> : null}
 
-        {/* MIN & MAX TEMP */}
+          {/* MIN & MAX TEMP */}
 
-        <div className="min-max mb-3">
-          {data.weather ? <p>{data.weather[0].main}</p> : null}
-          {data.main ? <p>Min: {(5/9 * (data.main.temp_min - 32)).toFixed(0)} °C &nbsp;&nbsp; Max: {(5/9 * (data.main.temp_max - 32)).toFixed(0)} °C</p>: null}
-        </div>
+          <div className="min-max mb-3">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+            {data.main ? <p>Min: {(5/9 * (data.main.temp_min - 32)).toFixed(0)} °C &nbsp;&nbsp; Max: {(5/9 * (data.main.temp_max - 32)).toFixed(0)} °C</p>: null}
+          </div>
 
-        {/* INFO DIV */}
+          {/* INFO DIV */}
 
-        <div className="info flex w-96 justify-between mx-auto bg-slate-400 bg-opacity-30 py-2 px-8 rounded-xl pl-11">
-          {data.main ? <p className="info_pressure info_card">{(data.main.pressure / 100).toFixed(0)} %</p> : null}
-          {data.wind ? <p className="info_speed info_card">{(data.wind.speed).toFixed(0)} m/s</p> : null}
-          {data.main ? <p className="info_humidity info_card">{(data.main.humidity)} %</p> : null}
+          <div className="info flex w-96 justify-between mx-auto bg-slate-400 bg-opacity-30 py-2 px-8 rounded-xl pl-11">
+            {data.main ? <p className="info_pressure info_card">{(data.main.pressure / 100).toFixed(0)} %</p> : null}
+            {data.wind ? <p className="info_speed info_card">{(data.wind.speed).toFixed(0)} m/s</p> : null}
+            {data.main ? <p className="info_humidity info_card">{(data.main.humidity)} %</p> : null}
 
-        </div>
-        
+          </div>
+          
       </div>
     </div>
-  );
+  ));
 }
 
 export default App;
