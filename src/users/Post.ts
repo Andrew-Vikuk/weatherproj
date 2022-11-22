e694ee78b583fe14f68f39afc754224a821e99fb
Mykola <mykola.harazdii@gmail.com>
2022-11-22 16:13:09 +0200

    unique name

--- src/users/Post.ts
+++ src/users/Post.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const Post = new mongoose.Schema({
-  name: {type: String, required: true},
+  name: {type: String, required: true, unique: true},
   password: {type: String, required: true},
   type: {type: String, required: false}
 })
